f429ce4fca11afeb108e5a00d58c2f4c0b008cad
igorspestana <igorspestana@gmail.com>
2024-12-11 11:06:50 -0300

    Adicionado client rest e nova rota em app controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/exemple')
+  getExemple(): string {
+    return 'Exemplo de response';
+  }
 }
